42a802be03d2f0b90ae69aab60e2dba892f5afa2
Rownak Borhan Himel <53826751+3207-Rhims@users.noreply.github.com>
2021-08-05 13:14:08 +0600

    Add files via upload

--- urls.py
+++ urls.py
@@ -1,22 +1,12 @@
-"""Django_CRUD URL Configuration
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/2.0/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
-from django.contrib import admin
-from django.urls import path, include
+from django.urls import path
+from . import views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include('crud.urls')),
+    path('', views.home, name='home' ),
+    path('index/', views.index, name='index' ),
+    path('create/', views.create, name='create' ),
+    path('add_book/', views.add_book, name='add_book' ),
+    path('delete/<id>/', views.delete, name='delete' ),
+    path('edit/<id>/', views.edit, name='edit' ),
+    path('update/<id>/', views.update, name='update' ),
 ]
